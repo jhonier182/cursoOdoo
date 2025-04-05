84ea542eedd76dd181dc03cf8098d787a8b91d7b
YHONIER ARIAS <jhonier18249@gmail.com>
2025-04-05 09:42:37 -0500

    Corrige errores tipográficos en el modelo de propiedad inmobiliaria, establece valores predeterminados para la disponibilidad de fecha y el número de dormitorios, y añade un campo de ayuda en la vista de propiedades.

--- real_state/models/estate_property.py
+++ real_state/models/estate_property.py
@@ -1,20 +1,20 @@
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
     
     name = fields.Char(required=True)
-    descrption = fields.Text()
+    description = fields.Text()  
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
+    date_availability = fields.Date(default=lambda self: fields.Date.today() + relativedelta(months=3))
     selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
+    garage = fields.Boolean() 
+    garden = fields.Boolean()  
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([
         ('north', 'North'),
