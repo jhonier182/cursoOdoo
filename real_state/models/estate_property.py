d7af0bfa1b1522de7046aabfc476722ac1c17203
YHONIER ARIAS <jhonier18249@gmail.com>
2025-04-05 09:56:54 -0500

    Agrega campos 'active' y 'state' al modelo de propiedad inmobiliaria, actualiza las traducciones de las orientaciones del jardín y ajusta la propiedad 'selling_price' a solo lectura.

--- real_state/models/estate_property.py
+++ real_state/models/estate_property.py
@@ -6,10 +6,18 @@ class EstateProperty(models.Model):
     _description = 'Estate Property'
     
     name = fields.Char(required=True)
-    description = fields.Text()  
+    description = fields.Text()
+    active = fields.Boolean(default=True)  
+    state = fields.Selection([
+        ("new", "New"),
+        ("offer_received", "Offer Received"),
+        ("offer_accepted", "Offer Accepted"),
+        ("sold", "Sold"),
+        ("canceled", "Canceled")
+    ], default='new')
     postcode = fields.Char()
-    date_availability = fields.Date(default=lambda self: fields.Date.today() + relativedelta(months=3))
-    selling_price = fields.Float()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.today() + relativedelta(months=3))
+    selling_price = fields.Float(readonly=True, copy=False)  # readonly para activar o desactivar campos
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
@@ -17,8 +25,8 @@ class EstateProperty(models.Model):
     garden = fields.Boolean()  
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([
-        ('north', 'North'),
-        ('south', 'South'),
-        ('east', 'East'),
-        ('west', 'West')
+        ('north', 'Norte'),
+        ('south', 'Sur'),
+        ('east', 'Este'),
+        ('west', 'Oeste')
     ])
