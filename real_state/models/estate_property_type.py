65f2f74d03e0a69acf8da4377fce7e1b424e9b73
YHONIER ARIAS <jhonier18249@gmail.com>
2025-04-08 11:30:34 -0500

    Actualiza el modelo de tipo de propiedad para incluir el campo 'sequence', mejorando el orden de visualización. Se añade ayuda contextual en la vista de tipo de propiedad y se ajusta la búsqueda para incluir el campo 'sequence', optimizando la usabilidad en la interfaz.

--- real_state/models/estate_property_type.py
+++ real_state/models/estate_property_type.py
@@ -4,14 +4,13 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate Property Type"
-    _order = "name"
+    _order = "sequence,name"
 
     name = fields.Char(required=True)
     property_ids = fields.One2many("estate.property", "property_type_id")
+    sequence = fields.Integer()
 
     _sql_constraints = [
         ('unique_name', 'UNIQUE(name)', 'El nombre del tipo de propiedad debe ser único.')
     ]
-
-    name = fields.Char(required=True)
     
