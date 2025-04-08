0710e8a1fdc93d931f5f45ad2c5354281d82dd0d
YHONIER ARIAS <jhonier18249@gmail.com>
2025-04-08 08:57:51 -0500

    """ Actualiza el modelo de tipo de propiedad para incluir campos 'name' y 'property_ids', asegurando la integridad de los datos con restricciones SQL. Mejora la vista de formulario para mostrar el nombre del tipo de propiedad y una lista de propiedades asociadas, facilitando la gestión y visualización de la información en la interfaz. """

--- real_state/models/estate_property_type.py
+++ real_state/models/estate_property_type.py
@@ -5,7 +5,9 @@ class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate Property Type"
 
-    # Restricciones SQL para garantizar integridad de datos
+    name = fields.Char(required=True)
+    property_ids = fields.One2many("estate.property", "property_type_id")
+
     _sql_constraints = [
         ('unique_name', 'UNIQUE(name)', 'El nombre del tipo de propiedad debe ser único.')
     ]
